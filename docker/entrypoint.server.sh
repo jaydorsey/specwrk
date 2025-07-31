1de7bd3242221bf18691c7dab0605a42c9efd4c3
Daniel Westendorf <daniel@prowestech.com>
2025-07-31 11:25:14 -0600

    Support specifying puma worker processes

--- docker/entrypoint.server.sh
+++ docker/entrypoint.server.sh
@@ -4,4 +4,4 @@ export THRUSTER_HTTP_PORT=${PORT:-5138}
 export THRUSTER_TARGET_PORT=3000
 export THRUSTER_HTTP_IDLE_TIMEOUT=${IDLE_TIMEOUT:-305}
 
-exec thrust puma --workers 0 --bind tcp://127.0.0.1:3000 --threads ${PUMA_THREADS:-1}
+exec thrust puma --workers 0 --bind tcp://127.0.0.1:3000 --threads ${PUMA_THREADS:-1} --workers ${PUMA_WORKERS:-0}
