dfd1d4eb44bf3b0c14abffb4424a0aeeeb870b43
Daniel Westendorf <daniel@prowestech.com>
2025-08-26 11:48:36 -0600

    Test Store.adapter_klass

--- spec/specwrk/store_spec.rb
+++ spec/specwrk/store_spec.rb
@@ -6,81 +6,119 @@ require "securerandom"
 require "specwrk/store"
 
 RSpec.describe Specwrk::Store do
-  let(:uri_string) { "file://#{Dir.tmpdir}" }
-  let(:scope) { SecureRandom.uuid }
+  describe ".adapter_klass" do
+    subject { described_class.adapter_klass(uri) }
 
-  let(:instance) { described_class.new(uri_string, scope) }
+    context "memory" do
+      let(:uri) { URI("memory://foobar") }
 
-  before { instance.clear }
-
-  describe "#[]" do
-    subject { instance[:foo] }
-
-    before do
-      instance[:foo] = "bar"
+      it { is_expected.to eq(Specwrk::Store::MemoryAdapter) }
     end
 
-    it { is_expected.to eq("bar") }
-  end
+    context "file" do
+      let(:uri) { URI("file://foo/bar") }
 
-  describe "#[]=" do
-    subject { instance[:foo] = 42 }
-
-    it { expect { subject }.to change { instance[:foo] }.from(nil).to(42) }
-  end
-
-  describe "#keys" do
-    subject { instance.keys }
-
-    before do
-      instance["a"] = 1
-      instance["____secret"] = 2
-      instance["b"] = 3
+      it { is_expected.to eq(Specwrk::Store::FileAdapter) }
     end
 
-    it { is_expected.to eq(%w[a b]) }
-  end
+    context "redis" do
+      let(:uri) { URI("redis://foobar") }
 
-  describe "#length" do
-    subject { instance.length }
+      before do
+        stub_const("Specwrk::Store::RedisAdapter", Class.new)
+      end
 
-    before { instance.merge!(x: 1, y: 2) }
-
-    it { is_expected.to eq(2) }
-  end
-
-  describe "#delete" do
-    subject { instance.delete("key") }
-
-    before { instance["key"] = "val" }
-
-    it { expect { subject }.to change(instance, :keys).from(["key"]).to([]) }
-  end
-
-  describe "#merge!" do
-    subject { instance.merge!(:alpha => 1, "beta" => 2) }
-
-    before { instance["alpha"] = 0 }
-
-    it { expect { subject }.to change(instance, :inspect).from(alpha: 0).to(alpha: 1, beta: 2) }
-  end
-
-  describe "#clear" do
-    subject { instance.clear }
-
-    before { instance.merge!("foo" => 1, "baz" => 2) }
-
-    it { expect { subject }.to change(instance, :keys).from(match_array(["foo", "baz"])).to([]) }
-  end
-
-  describe "#inspect" do
-    subject { instance.inspect }
-
-    before do
-      instance.merge!("foo" => 1, "baz" => 2)
+      it { is_expected.to eq(Specwrk::Store::RedisAdapter) }
     end
 
-    it { is_expected.to eq(foo: 1, baz: 2) }
+    context "rediss" do
+      let(:uri) { URI("rediss://foobar") }
+
+      before do
+        stub_const("Specwrk::Store::RedisAdapter", Class.new)
+      end
+
+      it { is_expected.to eq(Specwrk::Store::RedisAdapter) }
+    end
+  end
+
+  describe "instance methods" do
+    let(:uri_string) { "file://#{Dir.tmpdir}" }
+    let(:scope) { SecureRandom.uuid }
+
+    let(:instance) { described_class.new(uri_string, scope) }
+
+    before { instance.clear }
+
+    describe "#[]" do
+      subject { instance[:foo] }
+
+      before do
+        instance[:foo] = "bar"
+      end
+
+      it { is_expected.to eq("bar") }
+    end
+
+    describe "#[]=" do
+      subject { instance[:foo] = 42 }
+
+      it { expect { subject }.to change { instance[:foo] }.from(nil).to(42) }
+    end
+
+    describe "#keys" do
+      subject { instance.keys }
+
+      before do
+        instance["a"] = 1
+        instance["____secret"] = 2
+        instance["b"] = 3
+      end
+
+      it { is_expected.to eq(%w[a b]) }
+    end
+
+    describe "#length" do
+      subject { instance.length }
+
+      before { instance.merge!(x: 1, y: 2) }
+
+      it { is_expected.to eq(2) }
+    end
+
+    describe "#delete" do
+      subject { instance.delete("key") }
+
+      before { instance["key"] = "val" }
+
+      it { expect { subject }.to change(instance, :keys).from(["key"]).to([]) }
+    end
+
+    describe "#merge!" do
+      subject { instance.merge!(:alpha => 1, "beta" => 2) }
+
+      before { instance["alpha"] = 0 }
+
+      it { expect { subject }.to change(instance, :inspect).from(alpha: 0).to(alpha: 1, beta: 2) }
+    end
+
+    describe "#clear" do
+      subject { instance.clear }
+
+      before { instance.merge!("foo" => 1, "baz" => 2) }
+
+      it { expect { subject }.to change(instance, :keys).from(match_array(["foo", "baz"])).to([]) }
+    end
+
+    describe "#inspect" do
+      subject { instance.inspect }
+
+      before do
+        instance.merge!("foo" => 1, "baz" => 2)
+      end
+
+      it { is_expected.to eq(foo: 1, baz: 2) }
+    end
   end
 end
 
